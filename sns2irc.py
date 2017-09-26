3b9ce150e981164305b44d0605171173cab5e871
Sean Coates <sean@seancoates.com>
2017-09-26 14:29:11 -0400

    support for kwargs (zappa branch)

--- sns2irc.py
+++ sns2irc.py
@@ -12,6 +12,12 @@ IRC_PASS = os.environ["IRC_PASS"]
 def sns2irc(event, context):
     sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     sock.connect((IRC_HOST, IRC_PORT))
+
+    if 'kwargs' in event:
+        channel = event['kwargs'].get('channel', IRC_CHAN)
+    else:
+        channel = IRC_CHAN
+
     if IRC_SSL:
         irc = ssl.wrap_socket(sock)
     else:
@@ -23,14 +29,14 @@ def sns2irc(event, context):
     try:
         msg = event["Records"][0]["Sns"]["Subject"]
         if msg:
-            irc.send(str.encode('PRIVMSG #{} :{}\r\n'.format(IRC_CHAN, msg[:400])))
+            irc.send(str.encode('PRIVMSG #{} :{}\r\n'.format(channel, msg[:400])))
     except KeyError:
         pass
 
     try:
         msg = event["Records"][0]["Sns"]["Message"].strip("\n").replace("\n", " - ")
         if msg:
-            irc.send(str.encode('PRIVMSG #%s :%s\r\n' % (IRC_CHAN, msg[:400])))
+            irc.send(str.encode('PRIVMSG #%s :%s\r\n' % (channel, msg[:400])))
     except KeyError:
         pass
 
