ac8d348b06ab133fd5646f5a52a80d5f3043ef9b
Sean Coates <sean@seancoates.com>
2017-09-27 14:56:47 -0400

    rework for the irc client instead of rawish socket

--- sns2irc.py
+++ sns2irc.py
@@ -1,6 +1,9 @@
-import socket, ssl
+import irc.client, irc.connection, ssl
 from ast import literal_eval
 import os
+from time import sleep
+
+import logging
 
 IRC_NICK = os.environ["IRC_NICK"]
 IRC_HOST = os.environ["IRC_HOST"]
@@ -10,33 +13,66 @@ IRC_CHAN = os.environ["IRC_CHAN"]
 IRC_PASS = os.environ["IRC_PASS"]
 
 def sns2irc(event, context):
-    sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    sock.connect((IRC_HOST, IRC_PORT))
+
+    request_id = context.aws_request_id
+
+    alt_nick = IRC_NICK + "-" + request_id.split('-')[0]
+
+    logging.debug("Event:")
+    logging.debug(event["Records"][0]["Sns"])
 
     if 'kwargs' in event:
-        channel = event['kwargs'].get('channel', IRC_CHAN)
+        channel = "#" + event['kwargs'].get('channel', IRC_CHAN)
     else:
-        channel = IRC_CHAN
+        channel = "#" + IRC_CHAN
+
+    logging.debug("Sending to {}".format(channel))
 
     if IRC_SSL:
-        irc = ssl.wrap_socket(sock)
+        factory = irc.connection.Factory(wrapper=ssl.wrap_socket)
     else:
-        irc = sock
-    irc.send(str.encode('PASS {}\r\n'.format(IRC_PASS)))
-    irc.send(str.encode('NICK {}\r\n'.format(IRC_NICK)))
-    irc.send(str.encode('USER {} {} {} :IRC2SNS\r\n'.format(IRC_NICK, IRC_NICK, IRC_NICK)))
-
+        factory = None
+    reactor = irc.client.Reactor()
     try:
-        msg = event["Records"][0]["Sns"]["Subject"]
-        if msg:
-            irc.send(str.encode('PRIVMSG #{} :{}\r\n'.format(channel, msg[:400])))
-    except KeyError:
-        pass
+        irc_client = reactor.server().connect(
+            IRC_HOST,
+            IRC_PORT,
+            IRC_NICK,
+            password=IRC_PASS,
+            connect_factory=factory,
+        )
+    except irc.client.ServerConnectionError:
+        print(sys.exc_info()[1])
+        return
 
-    try:
-        msg = event["Records"][0]["Sns"]["Message"].strip("\n").replace("\n", " - ")
-        if msg:
-            irc.send(str.encode('PRIVMSG #%s :%s\r\n' % (channel, msg[:400])))
-    except KeyError:
-        pass
+    def on_connect(connection, irc_event):
 
+        try:
+            msg = event["Records"][0]["Sns"]["Subject"]
+            if msg:
+                logging.debug("Subject> {}".format('{} :{}\r\n'.format(channel, msg[:400])))
+                connection.privmsg(channel, msg)
+        except KeyError:
+            pass
+
+        try:
+            msg = event["Records"][0]["Sns"]["Message"].strip("\n").replace("\n", " - ")
+            if msg:
+                logging.debug("Message> {}".format('{} :{}\r\n'.format(channel, msg[:400])))
+                connection.privmsg(channel, msg)
+        except KeyError:
+            pass
+
+        connection.quit()
+
+    def use_alt_nick(connection, irc_event):
+       connection.nick(alt_nick)
+
+    irc_client.add_global_handler("welcome", on_connect)
+    irc_client.add_global_handler("nicknameinuse", use_alt_nick)
+
+    for x in range(1, 10):
+        reactor.process_once()
+        sleep(0.2)
+
+    return
